e3e5b954676d14da9cb3591f5f026a014117c256
José Augusto <joseaugustolins@gmail.com>
2019-04-05 00:46:42 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
+function App(){
+
+ 
+  const [funcionarios, setFuncionarios] = useState([]);
+
+  useEffect( () => {
+    async function loadFunc(){
+      const res =  await axios.get('http://localhost:8080/api/funcionario/all');
+      setFuncionarios(res.data);
+    }
+    loadFunc();
+    console.log(funcionarios)
+  }, []);
+
+
+
+  
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {funcionarios.map((func,i)=>
+          <h1 key={i}>{i} - {func.nome}</h1>
+        )}        
       </div>
-    );
+    ) 
   }
-}
 
 export default App;
